a15b03d43f6d95c7d82bc300c937fe2aaa59fcf6
Jean-Philippe Prade <jean-philippe.prade@alten.com>
2021-06-21 11:21:20 +0200

    FIX : integer divided by integer give integer: so 0,x value id rounded to 0 making Text (Class=4, Element=4) to be invisible

--- src/net/sf/jcgm/core/TextCommand.java
+++ src/net/sf/jcgm/core/TextCommand.java
@@ -126,7 +126,7 @@ public abstract class TextCommand extends Command {
 			screenResolution = Toolkit.getDefaultToolkit()
 					.getScreenResolution();
 		}
-		double height = fontMetrics.getAscent() * 72 / screenResolution;
+		double height = fontMetrics.getAscent() * 72 / (double) screenResolution;
 
 		scaleText(d, fontMetrics, glyphVector, logicalBounds.getWidth(), height);
 
